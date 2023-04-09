35d4836f64acd4d2f9d3d349e21311c27aa324d9
Matthew Whited <matt@whited.us>
2023-04-09 15:00:17 -0400

    array play

--- Hours15/Arrays/Arrays.cpp
+++ Hours15/Arrays/Arrays.cpp
@@ -4,5 +4,29 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	int arr[5];	
+
+	for (int i = 0; i < 5; i++) {
+		arr[i] = 1 << i;
+		cout << "i:" << i << "\tv:" << arr[i] << endl;
+	}
+
+	for (int i = 4; i >= 0; i--) {
+		cout << "i:" << i << "\tv:" << arr[i] << endl;
+	}
+
+	{
+		int i = 50;
+		cout << "i:" << i << "\tv:" << arr[i] << endl;
+	}
+
+	int len = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "arr len:" << len << endl;
+
+	//Note: this is intentionally over shooting 
+	for (int i = 0; i < 100; i++) {
+		arr[i] = 1 << i;
+		cout << "i:" << i << "\tv:" << arr[i] << endl;
+	}
 }
