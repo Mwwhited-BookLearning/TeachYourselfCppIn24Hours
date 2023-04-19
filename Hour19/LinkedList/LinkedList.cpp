e4bbafeec7c27a2e78974e54feab41d1eff7ca38
Matthew Whited <matt@whited.us>
2023-04-18 21:08:06 -0400

    dont worry about linked lists

---
+++ Hour19/LinkedList/LinkedList.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+//#include <>
+
+using namespace std;
+
+int main()
+{//Just use standard libraries... you could implment raw but why?
+
+
+	std::cout << "Hello World!\n";
+
+	stack<short> stack;
+
+	queue<long> queue;
+
+	list<int> listOfInts;
+	listOfInts.push_back(1);
+	listOfInts.push_back(2);
+	listOfInts.push_back(3);
+	listOfInts.push_back(4);
+
+	listOfInts.
+}
